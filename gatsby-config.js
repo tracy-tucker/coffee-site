015fc453997d199a8696d045847e1ce68df628ae
Tracy Tucker <tracytuckeremail@gmail.com>
2022-03-09 12:54:23 -0600

    Installations and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,33 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Coffee Shop`,
+    description: `Gatsby Airtable Example. Built using Airtable, Algolia Search, Gatsby Background Image plugin and  React Context API. Containts two sliders, real-time Airtable updates and submenus. Styled using Styled-Components.`,
+    author: `@tracytucker`,
+    titleTemplate: `%s | Gatsby - Airtable`,
+    url: `come back to this one`,
+    image: `come back to this one`,
+    twitterUsername: `@mapTracy`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }

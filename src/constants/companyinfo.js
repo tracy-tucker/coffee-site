b96cae1d5c8ae27a3b268fa0cd9725e7053da640
Tracy Tucker <tracytuckeremail@gmail.com>
2022-03-10 12:47:56 -0600

    staging companyinfo array

---
+++ src/constants/companyinfo.js
@@ -0,0 +1,34 @@
+import React from "react"
+import {
+  GiCoffeeCup,
+  GiHolyGrail,
+  GiStabbedNote,
+  GiBriefCase,
+} from "react-icons/gi"
+
+export default [
+  {
+    id: 1,
+    icon: <GiCoffeeCup className="icon" />,
+    label: "mission",
+    text: "Activated charcoal post-ironic unicorn flexitarian tumeric, direct trade man bun mumblecore kickstarter art party.",
+  },
+  {
+    id: 2,
+    icon: <GiHolyGrail className="icon" />,
+    label: "vision",
+    text: "Activated charcoal post-ironic unicorn flexitarian tumeric, direct trade man bun mumblecore kickstarter art party.",
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote className="icon" />,
+    label: "history",
+    text: "Activated charcoal post-ironic unicorn flexitarian tumeric, direct trade man bun mumblecore kickstarter art party.",
+  },
+  {
+    id: 4,
+    icon: <GiBriefCase className="icon" />,
+    label: "work",
+    text: "Activated charcoal post-ironic unicorn flexitarian tumeric, direct trade man bun mumblecore kickstarter art party.",
+  },
+]

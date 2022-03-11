3d6146ffa9434cce8507b4fabcfbc938611c3c56
Tracy Tucker <tracytuckeremail@gmail.com>
2022-03-10 18:20:00 -0600

    Creatin index comp to export all components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Layout from "./Layout"
+import Hero from "./Hero"
+import About from "./About"
+import Coffees from "./Coffees"
+import Survey from "./Survey"
+import Slider from "./Slider"
+import Algolia from "./Algolia"
+import GridCoffees from "./GridCoffees"
+
+export { Layout, Hero, About, Coffees, Survey, Slider, Algolia, GridCoffees }
